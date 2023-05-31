855c4a0978e843ae6c8d0b3120bb3e70fb61ba25
jaehan-Bae <yanggo31@gmail.com>
2023-05-31 22:43:06 +0900

    wow추가

--- js/custom.js
+++ js/custom.js
@@ -8,13 +8,22 @@ $(function () {
     }
   });
 
-  $('.recommend-list > div').click(function(){
+  /* recommend tab-menu */
+  $('.recommend-list > div').click(function () {
     $(this).addClass('active');
     $(this).siblings().removeClass('active');
 
     $('.recommend-con').removeClass('active');
     $('#' + $(this).attr('data-alt')).addClass('active');
   });
+
+  /* Wow Scroll Reveal Animation */
+  wow = new WOW({
+    boxClass: 'wow',
+    offset: 150,
+    mobile: true
+  })
+  wow.init()
 });
 
 
