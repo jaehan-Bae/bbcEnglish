c5937f5197494c0269cbf4b8625de7b62ce0a732
jaehan-Bae <yanggo31@gmail.com>
2023-06-01 09:29:20 +0900

    goto_top추가

--- js/custom.js
+++ js/custom.js
@@ -2,9 +2,9 @@ $(function () {
   /* Header Scroll Change */
   $(window).scroll(function () {
     if ($(window).scrollTop() > 60) {
-      $('header').addClass('active');
+      $('header, .goto').addClass('active');
     } else {
-      $('header').removeClass('active');
+      $('header, .goto').removeClass('active');
     }
   });
 
